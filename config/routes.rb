d0f0d416ea98fad5520da1d987fb00fc07cae45e
Luke A Chase <chase.luke.a@gmail.com>
2016-01-19 18:33:54 -0600

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  get 'home/index'
 
-  get 'home/about'
+  get '/about', to: 'home#about', as: 'about'
+  
+  root to: 'home#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
